d3f03aa32b2c5b6f74a5ff9d94c109cffe63bb08
MartinQuesquen <martinquesquenpa3@gmail.com>
2025-01-02 23:30:12 -0500

    add html and function export table

--- README.md
+++ README.md
@@ -3,26 +3,26 @@
 Pull Request Exporter from Jira to Excel
 This project addresses the need to generate a list of pull requests made on the Jira platform, as it does not provide a native option to export tables to an Excel file.
 
-Description
+# Description
 A solution was implemented that leverages the platform's capabilities to perform a request that returns data in JSON format. This data was processed and displayed in an HTML file. Additionally, a function was developed to export all the information obtained from Jira to an Excel file, providing an efficient and tailored solution for this use case.
 
-Features
+# Features
 • Retrieve data from Jira through a request that returns a JSON.
 • Process and display the data in a web environment (HTML).
 • Directly export the processed information to an Excel file.
 • Practical and adaptable solution for similar needs.
 
-Usage
+# Usage
 • Perform the initial configuration to fetch JSON data from Jira.
 • View the processed data in the HTML environment.
 • Use the export function to generate an Excel file with the required information.
 
-Requirements
+# Requirements
 • Access to the Jira API to make requests and fetch JSON data.
 • Store the JSON data in an array.
 • A compatible browser to view the HTML table.
 • View the project on a server (for this development, the "Live Server" extension from Visual Studio Code was used).
 • Necessary dependencies for exporting data to Excel (these may vary depending on the implementation).
 
-Additional Notes
+# Additional Notes
 This project was designed as a specific solution to export data from Jira, but it can be adapted to other cases where processing and exporting information from a JSON source is required.
---
+++ const.js
@@ -0,0 +1,138 @@
+export const dataOBJ = 
+[
+    {
+        "comment_count": 0,
+        "task_count": 0,
+        "type": "pullrequest",
+        "id": 1325,
+        "title": "resolve conflict random logout",
+        "state": "MERGED",
+        "merge_commit": {
+            "hash": "7a71ad6f5c1aw",
+            "links": {
+                "self": {
+                    "href": "url_link_self"
+                },
+                "html": {
+                    "href": "url_ref_html"
+                }
+            },
+            "type": "commit"
+        },
+        "diff_type": 2,
+        "latest_excluded_files_version": "",
+        "close_source_branch": true,
+        "author": {
+            "display_name": "Martin Quesquen",
+            "links": {
+                "self": {
+                    "href": "url_link_self"
+                },
+                "avatar": {
+                    "href": "url_ref_image_avatar"
+                },
+                "html": {
+                    "href": "url_ref_html"
+                }
+            },
+            "type": "user",
+            "uuid": "{44653ac1d1-88gg-4eae-1as5d-16adsg5vca}",
+            "account_id": "468fasc4as6f51asv5ads41f6as",
+            "nickname": "Martin Quesquen"
+        },
+        "created_on": "2024-12-20T17:13:31.337002+00:00",
+        "updated_on": "2024-12-20T17:14:43.143326+00:00",
+        "closed_on": "2024-12-20T17:14:43.128725+00:00",
+        "destination": {
+            "branch": {
+                "name": "master"
+            },
+            "repository": {
+                "type": "repository",
+                "full_name": "myproject",
+                "links": {
+                    "self": {
+                        "href": "url_link_self"
+                    },
+                    "html": {
+                        "href": "url_ref_html"
+                    },
+                    "avatar": {
+                        "href": "url_ref_image_avatar"
+                    }
+                },
+                "name": "myproject",
+                "uuid": "{bcf2202d-ceb3-4238-9391-1d2ab5f255a4}"
+            }
+        },
+        "source": {
+            "branch": {
+                "name": "hotfix/APIS-3495-bug-random-logout"
+            },
+            "commit": {
+                "hash": "156aef16asc4",
+                "links": {
+                    "self": {
+                        "href": "url_link_self"
+                    },
+                    "html": {
+                        "href": "url_ref_html"
+                    }
+                },
+                "type": "commit"
+            },
+            "repository": {
+                "type": "repository",
+                "full_name": "name/project",
+                "links": {
+                    "self": {
+                        "href": "url_link_self"
+                    },
+                    "html": {
+                        "href": "url_ref_html"
+                    },
+                    "avatar": {
+                        "href": "url_ref_image_avatar"
+                    }
+                },
+                "name": "myproject",
+                "uuid": "{asd1158f3-gb65-8403-9391-asd1c8fg1}"
+            }
+        },
+        "participants": [
+            {
+                "type": "participant",
+                "user": {
+                    "display_name": "name.User",
+                    "links": {
+                        "self": {
+                            "href": "url_ref_links"
+                        },
+                        "avatar": {
+                            "href": "url_ref_image_avatar"
+                        },
+                        "html": {
+                            "href": "url_html_user"
+                        }
+                    },
+                    "type": "user",
+                    "uuid": "{122554-444dvd-f41ds-1asf114ggv48}",
+                    "account_id": "123456789-987654321-1597534680"
+                },
+                "role": "REVIEWER",
+                "approved": true,
+                "state": "approved",
+                "participated_on": "2024-12-20T17:14:35.055934+00:00"
+            }
+        ],
+        "links": {
+            "self": {
+                "href": "url_prueb_link_self"
+            },
+            "html": {
+                "href": "url_prueb_html"
+            }
+        },
+        "merge_in_progress": null
+    }
+]
